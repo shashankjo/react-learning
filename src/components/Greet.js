539419daf5a2bf32c5d11ffa7879a0c068924868
shashank <shashank@tchowklabs.com>
2020-11-22 18:29:22 +0530

    Functinal and Class components examples

---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet() {
+//  return <h1> Hello Shashank is learning React</h1>
+// }
+
+const Greet = () =>  <h1> Hello Shashank!! Functional component</h1>
+export default Greet
