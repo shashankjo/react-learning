e79a8b29e61223469c4d4828d72212dc2603341b
unknown <kumarshashank.jo@gmail.com>
2021-07-13 12:52:50 +0530

    List rendering with array of hashes

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
 export function Namelist() {
-    const names = [ 'Bruce', 'Clark', 'Diana']
-    const namesList = names.map(name => <div> {name} </div>)
+    const persons = [ 
+        {
+            id: 1,
+            name: 'Bruce',
+            age: 30,
+            skill: 'React'
+         },
+         {
+            id: 2,
+            name: 'Clark',
+            age: 31,
+            skill: 'Angular'
+         },
+         {
+            id: 3,
+            name: 'Diana',
+            age: 31,
+            skill: 'Vue'
+         } 
+        ]
+    const personList = persons.map(person => 
+    <h2> I am {person.name}. I am {person.age} years old. I know {person.skill} </h2>)
 
     return (
-       <div> {namesList} </div>
+       <div> {personList} </div>
     )
 }
